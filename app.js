3247e8e375598e62f8338a5b9a0c44b6afbbf0c8
swyzsh <swyzsh@protonmail.com>
2024-03-09 08:44:34 +0530

    add links

--- app.js
+++ app.js
@@ -5,3 +5,17 @@
 3. Use apple emojis for hands
 
 */
+
+// run script after DOM loads
+document.addEventListener('DOMContentLoaded', function() {
+  // socials
+  const twitterBtn = document.getElementById('twitter-btn');
+  twitterBtn.addEventListener('click', function() {
+    window.open('https://x.com/swyzsh/', '_blank');
+  });
+  const githubBtn = document.getElementById('github-btn');
+  githubBtn.addEventListener('click', function() {
+    window.open('https://github.com/swyzsh', '_blank');
+  });
+})
+
