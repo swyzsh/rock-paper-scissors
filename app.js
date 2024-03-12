ff3cc20e6ee088b22cb36f650f1d49f2665fdaef
Swyzsh <swyzsh@protonmail.com>
2024-03-12 18:39:05 +0530

    init new layout for hand selection

--- app.js
+++ app.js
@@ -4,6 +4,13 @@
 2. change change card color depending on result of game
 3. Use apple emojis for hands
 
+get bot to gen random number from 1-3 => assing each number to a type (rock, paper scisscors)
+
+write game lose/win logic by comparing types from bot and user
+let user select hand user wants to play with
+
+4. give some sort of visual clue for winning/losing/draw
+
 */
 
 // run script after DOM loads
@@ -17,4 +24,13 @@ document.addEventListener('DOMContentLoaded', function() {
   githubBtn.addEventListener('click', function() {
     window.open('https://github.com/swyzsh', '_blank');
   });
-})
+
+  // begin game with push of button
+  const startBtn = document.getElementById('start-button');
+  startBtn.addEventListener('click', startGame);
+});
+
+// game logic
+function startGame() {
+  console.log(`Game has started`);
+}
